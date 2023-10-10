d2334e15f8db58a4623c9a7e146bd8af776abdf7
sebadio <sebaingame@gmail.com>
2023-10-09 22:20:01 -0300

    FormCheckout now has a function that handlesCheckout

--- src/lib/FormCheckout.svelte
+++ src/lib/FormCheckout.svelte
@@ -1,20 +1,71 @@
-<script></script>
+<script>
+  import { ip } from "../helpers/writables";
+  let name = "";
+  let email = "";
+  let address = "";
+  let city = "";
+  let state = "";
+  let zip = "";
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // check if data is valid
+    if (
+      name === "" ||
+      email === "" ||
+      address === "" ||
+      city === "" ||
+      state === "" ||
+      zip === ""
+    ) {
+      alert("Please fill out all fields");
+      return;
+    }
+
+    const data = {
+      name,
+      email,
+      address,
+      city,
+      state,
+      zip,
+    };
+
+    const res = await fetch(`${ip}/checkout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const json = await res.json();
+    console.log(json);
+  };
+</script>
 
 <div class="formCheckout">
   <h2>Checkout</h2>
-  <form>
+  <form method="post" on:submit={(e) => submitForm(e)}>
     <label for="name">Name</label>
-    <input type="text" id="name" />
+    <input bind:value={name} type="text" id="name" />
+
     <label for="email">Email</label>
-    <input type="email" id="email" />
+    <input bind:value={email} type="email" id="email" />
+
     <label for="address">Address</label>
-    <input type="text" id="address" />
+    <input bind:value={address} type="text" id="address" />
+
     <label for="city">City</label>
-    <input type="text" id="city" />
+    <input bind:value={city} type="text" id="city" />
+
     <label for="state">State</label>
-    <input type="text" id="state" />
+    <input bind:value={state} type="text" id="state" />
+
     <label for="zip">Zip</label>
-    <input type="text" id="zip" />
+    <input bind:value={zip} type="text" id="zip" />
+
     <button type="submit">Submit</button>
   </form>
 </div>
