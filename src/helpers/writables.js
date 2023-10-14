91bf597313b8c8ac512bed3fd8ca6d5f814d495a
sebadio <sebaingame@gmail.com>
2023-10-14 01:15:43 -0300

    updated gh pages

--- src/helpers/writables.js
+++ src/helpers/writables.js
@@ -7,5 +7,5 @@ export const authenticated = writable(false);
 export const choferes = writable([]);
 export let user = writable("");
 export let tfa = writable(false);
-// export const client = axios.create({ baseURL: "https://tienda-obli.sebasdiaz.com" })
-export const client = axios.create({ baseURL: "https://localhost" })
+export const client = axios.create({ baseURL: "https://tienda-obli.sebasdiaz.com" })
+// export const client = axios.create({ baseURL: "https://localhost" })
